df30d400277b1670f6ff7ecb258d97166d0b1677
Shital <badki_shital@yahoo.com>
2023-02-23 17:20:51 +0800

    added ratelimite example

---
+++ rate-limiter-example/src/main/java/com/ratelimit/others/SlidingWindowLog.java
@@ -0,0 +1,26 @@
+package com.ratelimit.others;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SlidingWindowLog extends RateLimiter {
+
+  private final Queue<Long> log = new LinkedList<>();
+
+  protected SlidingWindowLog(int maxRequestPerSec) {
+    super(maxRequestPerSec);
+  }
+
+  @Override
+  boolean allow() {
+    long curTime = System.currentTimeMillis();
+    long boundary = curTime - 1000;
+    synchronized (log) {
+      while (!log.isEmpty() && log.element() <= boundary) {
+        log.poll();
+      }
+      log.add(curTime);
+      return log.size() <= maxRequestPerSec;
+    }
+  }
+}
